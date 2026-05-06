91acd484dd227685f7ef167798e63b3606320fc7
Ivan Cunyas <ivanruben.cr@gmail.com>
2026-05-06 01:38:35 -0500

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/mentoredu/MentoreduApiApplication.java
@@ -0,0 +1,13 @@
+package com.mentoredu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoreduApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoreduApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mentoredu/MentoreduApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoredu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoreduApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
