48063569cf3cd74b228f7905d3e674b5b6b8c7fc
Alvaro <alvaroa123123@gmail.com>
2026-05-06 23:15:21 -0500

    fix: corregir @JsonIgnore que bloqueaba deserialización del password

--- src/main/java/com/mentoredu/auth/model/User.java
+++ src/main/java/com/mentoredu/auth/model/User.java
@@ -1,6 +1,6 @@
 package com.mentoredu.auth.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -26,7 +26,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false)
     private String password;
 
