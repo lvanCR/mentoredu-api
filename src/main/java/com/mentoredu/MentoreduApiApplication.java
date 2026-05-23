047afa91b973e88b5bc8d5e19aaa2910bce6ddc5
Ivan Cunyas <ivanruben.cr@gmail.com>
2026-05-23 14:42:56 -0500

    fix: render desploy

--- src/main/java/com/mentoredu/MentoreduApiApplication.java
+++ src/main/java/com/mentoredu/MentoreduApiApplication.java
@@ -3,7 +3,7 @@ package com.mentoredu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mentoredu")
 public class MentoreduApiApplication {
 
     public static void main(String[] args) {
