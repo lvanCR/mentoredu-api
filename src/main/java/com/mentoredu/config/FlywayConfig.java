98f1bf9dc321bcaa7ec74030d7f2b25b33245fef
Ivan Cunyas <ivanruben.cr@gmail.com>
2026-06-27 20:32:47 -0500

    add: spring ai

--- src/main/java/com/mentoredu/config/FlywayConfig.java
+++ src/main/java/com/mentoredu/config/FlywayConfig.java
@@ -23,6 +23,8 @@ public class FlywayConfig {
         Flyway flyway = Flyway.configure()
                 .dataSource(dataSource)
                 .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .baselineVersion("17")
                 .load();
         flyway.migrate();
         return flyway;
